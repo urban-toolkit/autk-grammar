bd066fc8827390b91f2afce5a034a6e8948e2430
Gustavo Moreira <m.muller.gustavo@gmail.com>
2026-05-21 11:35:13 -0500

    fixing lint errors

--- adapters/autk/eslint.config.js
+++ adapters/autk/eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js';
 import tsPlugin from '@typescript-eslint/eslint-plugin';
 import tsParser from '@typescript-eslint/parser';
 import prettier from 'eslint-config-prettier';
+import globals from 'globals';
 
 export default [
   { ignores: ['node_modules', 'dist'] },
@@ -9,10 +10,14 @@ export default [
   {
     files: ['**/*.ts'],
     plugins: { '@typescript-eslint': tsPlugin },
-    languageOptions: { parser: tsParser },
+    languageOptions: {
+      parser: tsParser,
+      globals: { ...globals.browser },
+    },
     rules: {
       ...tsPlugin.configs.recommended.rules,
       '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
     },
   },
   prettier,
